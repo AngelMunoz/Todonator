a79126339551e44d4cc59798653060086ba106f6
Angel D. Munoz <scythes.lol@gmail.com>
2019-07-16 08:27:52 -0600

    feature(): add unclosable internal frames

--- src/main/scala/me/tunaxor/todonator/frames/TodonatorDesktop.scala
+++ src/main/scala/me/tunaxor/todonator/frames/TodonatorDesktop.scala
@@ -2,10 +2,22 @@ package me.tunaxor.todonator.frames
 
 import javax.swing.JDesktopPane
 
-import scala.swing.{Component, Dimension}
+import scala.swing.{Component, Dimension, FlowPanel, Label}
 
 object TodonatorDesktop {
   val mainDesktop: Component = Component.wrap(new JDesktopPane())
+  private val _quickNotes = Component.wrap(new QuickNotesFrame(new FlowPanel {
+    contents += new Label("Hello Quick Notes!")
+  }))
+  private val _myLists = Component.wrap(new MyListsFrame(new FlowPanel {
+    contents += new Label("Hello Lists!")
+  }))
+  private val _settings = Component.wrap(new SettingsFrame(new FlowPanel {
+    contents += new Label("Hello Settings!")
+  }))
+  addFrameToMainDesktop("quickNotes", _quickNotes)
+  addFrameToMainDesktop("myLists", _myLists)
+  addFrameToMainDesktop("settings", _settings)
   mainDesktop.visible = true
 
   def newDesktop(preferredSize: Dimension): Component = {
@@ -18,7 +30,8 @@ object TodonatorDesktop {
     desktop.peer.add(frame.peer)
   }
 
-  def addFrameToMainDesktop(frame: Component) {
+  def addFrameToMainDesktop(key: String, frame: Component) {
     mainDesktop.peer.add(frame.peer)
   }
+
 }
